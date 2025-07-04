3a34ec48e8f77f1ee2e478eb9b4a184c14f99357
a0x13 <d0x13@outlook.com>
2025-07-04 18:22:40 +0800

    feat: 多线程遍历

--- main.cc
+++ main.cc
@@ -3,6 +3,10 @@
 #include <thread>
 #include <queue>
 #include <filesystem>
+#include <vector>
+#include <atomic>
+#include <algorithm>
+#include <numeric>
 
 #include <spdlog/spdlog.h>
 
@@ -23,7 +27,7 @@ void single_thread_travel(const std::string &path)
         dir_queue.pop();
 
         for (const auto &entry: fs::directory_iterator(dir.path())) {
-            spdlog::info("travel: {}", entry.path().c_str());
+            // spdlog::info("travel: {}", entry.path().c_str());
 
             if (entry.is_directory() && !entry.is_symlink()) {
                 count++;
@@ -32,7 +36,6 @@ void single_thread_travel(const std::string &path)
         }
     }
 
-    spdlog::info("---------------------------------------");
     spdlog::info("directory count: {}", count);
     
 }
@@ -77,6 +80,142 @@ void multi_thread_travel(const std::string &path)
     t2.join();
 }
 
+template<typename T>
+class SafeQueue
+{
+public:
+    SafeQueue() = default;
+
+    SafeQueue(SafeQueue &&other) noexcept {
+        std::lock_guard lock(other._mut);
+        _queue = std::move(other._queue);
+    }
+
+    SafeQueue& operator=(SafeQueue &&other) noexcept {
+        if (this != &other) {
+            std::scoped_lock scope(_mut, other._mut);
+            _queue = std::move(other._queue);
+        }
+
+        return *this;
+    }
+
+    void enqueue(const T &t) {
+        std::lock_guard lock(_mut);
+        _queue.emplace(t);
+    }
+
+    bool dequeue(T &t) {
+        std::lock_guard lock(_mut);
+        if (_queue.empty()) {
+            return false;
+        }
+        t = _queue.front();
+        _queue.pop();
+        return true;
+    }
+
+    inline size_t size() {
+        return _queue.size();
+    }
+
+private:
+    std::queue<T> _queue;
+    std::mutex _mut;
+};
+
+template<typename T>
+class QueueManger 
+{
+public:
+    QueueManger(int n) {
+        _queues.resize(n);
+        _idxs.resize(n);
+        std::iota(_idxs.begin(), _idxs.end(), 0);
+    }
+
+    void enqueue(const T &t) {
+        // 找到队列最小的加入队中
+        SafeQueue<T> &queue = get_min_queue();
+        queue.enqueue(t);
+        _count++;
+    }
+
+    bool dequeue(int idx, T &t, std::function<void(void)> on_dequeue_sucess) {
+        if (idx < 0 || idx >= _queues.size()) {
+            return false;
+        }
+
+        bool sucess = _queues[idx].dequeue(t);
+        if (sucess) {
+            on_dequeue_sucess();
+            _count--;
+        }
+        return sucess;
+    }
+
+    inline size_t size() {
+        return _count;
+    }
+
+private:
+    SafeQueue<T>& get_min_queue() {
+        for (int i = _idxs.size() - 1; i > 0; i--) {
+            int p = i / 2;
+            if (_queues[_idxs[p]].size() > _queues[_idxs[i]].size()) {
+                std::swap(_idxs[i], _idxs[p]);
+            }
+        }
+
+        return _queues[_idxs[0]];
+    }
+
+private:
+    std::vector<SafeQueue<T>> _queues;
+    std::vector<int> _idxs;
+    std::atomic<int> _count = 0;
+};
+
+void queue_travel(const std::string &path)
+{
+    TIMER_SCOPE_MS("queue travel");
+
+    int queue_num = 3;
+    std::atomic<int> signal = 0;
+    QueueManger<fs::directory_entry> qm(queue_num);
+    std::atomic<int> count;
+
+    qm.enqueue(fs::directory_entry(path));
+    auto travel = [&qm, &signal, &count](int idx) {
+        fs::directory_entry dir;
+        while (signal > 0 || qm.size() > 0) {
+            bool is_sucess = qm.dequeue(idx, dir, [&signal](){signal++;});
+            if (is_sucess) {
+                for (const auto &entry: fs::directory_iterator(dir.path())) {
+                    // spdlog::info("travel: {}", entry.path().c_str());
+
+                    if (entry.is_directory() && !entry.is_symlink()) {
+                        count++;
+                        qm.enqueue(entry);
+                    }
+                }
+                signal--;
+            }
+        }
+        
+    };
+
+    std::vector<std::thread> ths;
+    for (int i = 0; i < queue_num; i++) {
+        ths.emplace_back(travel, i);
+    }
+
+    for (auto &t: ths) {
+        t.join();
+    }
+    spdlog::info("directory num: {}", count);
+}
+
 
 int main(int argc, char **argv)
 {
@@ -97,7 +236,8 @@ int main(int argc, char **argv)
         return -1;
     }
     
-    // single_thread_travel(argv[1]);
-    multi_thread_travel(argv[1]);
+    single_thread_travel(argv[1]);
+    // multi_thread_travel(argv[1]);
+    queue_travel(argv[1]);
     return 0;
 }
