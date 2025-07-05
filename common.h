5eccd2970e00e68992ed91b9377cdf2c647b1940
a0x13 <d0x13@outlook.com>
2025-07-05 11:16:44 +0800

    feat: 优化计时器性能

--- common.h
+++ common.h
@@ -10,61 +10,52 @@ namespace fs = std::filesystem;
 namespace ch = std::chrono;
 
 
+enum class Precision {
+    ns = 0,
+    us,
+    ms,
+    s
+};
+
 /*
  *  计时器
  */
+template<Precision P = Precision::ns>
 class Timer {
 public:
-    enum class Precision {
-        ns = 0,
-        us,
-        ms,
-        s
-    };
-
-public:
-    Timer(std::string in_tag, Precision in_p = Precision::ns) 
-        : _tag(in_tag), _precision(in_p) {
-        _start = ch::high_resolution_clock::now();
-    }
+    Timer(std::string in_tag) : _tag(in_tag), _start(ch::steady_clock::now()) {}
 
     ~Timer() {
-        auto cost_ns = ch::high_resolution_clock::now() - _start;
-        int64_t count;
-        const char *unit;
-        switch (_precision) {
-        case Precision::us:
-            unit = "us";
-            count = ch::duration_cast<ch::microseconds>(cost_ns).count();
-            break;
-
-        case Precision::ms:
-            unit = "ms";
-            count = ch::duration_cast<ch::milliseconds>(cost_ns).count();
-            break;
-
-        case Precision::s:
-            unit = "s";
-            count = ch::duration_cast<ch::seconds>(cost_ns).count();
-            break;
-
-        default:
-            unit = "ns";
-            count = cost_ns.count();
-            break;
+        if (!spdlog::should_log(spdlog::level::debug)) {
+            return;
         }
+        auto cost_ns = ch::steady_clock::now() - _start;
+        auto [count, unit] = convert(cost_ns);
         spdlog::debug("[Timer] [{}] cost {} {}", _tag, count, unit);
     }
 
 private:
-    Precision _precision;
+    template<typename Dur>
+    static auto convert(Dur ns) noexcept {
+        if constexpr (P == Precision::ns) {
+            return std::pair{ns.count(), "ns"};
+        } else if constexpr (P == Precision::us) {
+            return std::pair{duration_cast<ch::microseconds>(ns).count(), "us"};
+        } else if constexpr (P == Precision::ms) {
+            return std::pair{duration_cast<ch::milliseconds>(ns).count(), "ms"};
+        } else {
+            return std::pair{duration_cast<ch::seconds>(ns).count(), "s"};
+        }
+    }
+
+private:
     std::string _tag;
-    ch::high_resolution_clock::time_point _start;
+    ch::steady_clock::time_point _start;
 };
 
 #define CONCAT2(x, y) x##y
 #define CONCAT(x, y) CONCAT2(x, y)
-#define TIMER_SCOPE_NS(tag) Timer CONCAT( __func__, __LINE__)(tag, Timer::Precision::ns);
-#define TIMER_SCOPE_US(tag) Timer CONCAT( __func__, __LINE__)(tag, Timer::Precision::us);
-#define TIMER_SCOPE_MS(tag) Timer CONCAT( __func__, __LINE__)(tag, Timer::Precision::ms);
-#define TIMER_SCOPE_S(tag) Timer CONCAT( __func__, __LINE__)(tag, Timer::Precision::s);
+#define TIMER_SCOPE_NS(tag) Timer<Precision::ns> CONCAT( __func__, __COUNTER__)(tag);
+#define TIMER_SCOPE_US(tag) Timer<Precision::us> CONCAT( __func__, __COUNTER__)(tag);
+#define TIMER_SCOPE_MS(tag) Timer<Precision::ms> CONCAT( __func__, __COUNTER__)(tag);
+#define TIMER_SCOPE_S(tag) Timer<Precision::s> CONCAT( __func__, __COUNTER__)(tag);
